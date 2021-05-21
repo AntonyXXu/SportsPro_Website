875215a2d0887872c64e6f458c7f7384a3c7d14f
Andy Zhu <andyzhu7568@gmail.com>
2021-05-21 17:07:57 -0600

    login page

---
+++ SportsPro/Models/DomainModels/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsPro.Models
+{
+    public class User : IdentityUser
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        [NotMapped]
+        public IList<string> RoleNames { get; set; }
+    }
+}
