4830e81d09bd1c217c45e6d92effc1e4307b719a
Matt <80971715+mattbeez@users.noreply.github.com>
2021-05-21 10:56:28 -0600

    Created Registration Controller, Registration Folder and GetCustomer Page.

---
+++ SportsPro/Controllers/RegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsPro.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
+namespace SportsPro.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private SportsProContext context { get; set; }
+        public RegistrationController(SportsProContext ctx)
+        {
+            context = ctx;
+        }
+
+        [Route("getcustomer")]
+        [HttpGet]
+        public IActionResult GetCustomer()
+        {
+            ViewBag.Customers = context.Customers.ToList();
+            return View();
+        }
+    }
+}
