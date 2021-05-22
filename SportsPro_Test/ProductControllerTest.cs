67d7050b7d3283398c156ff5a9cc93c1c1202ed3
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-22 14:50:17 -0600

    Create ProductControllerTests

---
+++ SportsPro_Test/ProductControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+using Moq;
+using SportsPro.Models;
+using SportsPro.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SportsPro_Test
+{
+    public class ProductControllerTest
+    {
+        [Fact]
+        public void Index_Returns_ViewAction()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void About_Returns_ViewAction()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.About();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
