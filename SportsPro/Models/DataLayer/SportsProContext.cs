a5e9881ed5800c822e14c8bb7726ceffce6be7cb
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-22 16:59:15 -0600

    Seed Country

--- SportsPro/Models/DataLayer/SportsProContext.cs
+++ SportsPro/Models/DataLayer/SportsProContext.cs
@@ -23,105 +23,107 @@ namespace SportsPro.Models
         {
             base.OnModelCreating(modelBuilder);
 
-            //modelBuilder.ApplyConfiguration(new SeedProduct());
+            modelBuilder.ApplyConfiguration(new SeedProduct());
+            modelBuilder.ApplyConfiguration(new SeedTechnician());
+            modelBuilder.ApplyConfiguration(new SeedCountry());
 
 
-            modelBuilder.Entity<Product>().HasData(
-                new Product
-                {
-                    ProductID = 1,
-                    ProductCode = "DRAFT10",
-                    Name = "Draft Manager 1.0",
-                    YearlyPrice = 4.99M,
-                    ReleaseDate = DateTime.Parse("2017-02-01")
-                },
-                new Product
-                {
-                    ProductID = 2,
-                    ProductCode = "DRAFT20",
-                    Name = "Draft Manager 2.0",
-                    YearlyPrice = 5.99M,
-                    ReleaseDate = DateTime.Parse("2019-07-15 00:00:00.000")
-                },
-                new Product
-                {
-                    ProductID = 3,
-                    ProductCode = "LEAG10",
-                    Name = "League Scheduler 1.0",
-                    YearlyPrice = 4.99M,
-                    ReleaseDate = DateTime.Parse("2016-05-01 00:00:00.000")
-                },
-                new Product
-                {
-                    ProductID = 4,
-                    ProductCode = "LEAGD10",
-                    Name = "League Scheduler Deluxe 1.0",
-                    YearlyPrice = 7.99M,
-                    ReleaseDate = DateTime.Parse("2016-08-01 00:00:00.000")
-                },
-                new Product
-                {
-                    ProductID = 5,
-                    ProductCode = "TEAM10",
-                    Name = "Team Manager 1.0",
-                    YearlyPrice = 4.99M,
-                    ReleaseDate = DateTime.Parse("2017-05-01 00:00:00.000")
-                },
-                new Product
-                {
-                    ProductID = 6,
-                    ProductCode = "TRNY10",
-                    Name = "Tournament Master 1.0",
-                    YearlyPrice = 4.99M,
-                    ReleaseDate = DateTime.Parse("2015-12-01 00:00:00.000")
-                },
-                new Product
-                {
-                    ProductID = 7,
-                    ProductCode = "TRNY20",
-                    Name = "Tournament Master 2.0",
-                    YearlyPrice = 5.99M,
-                    ReleaseDate = DateTime.Parse("2018-02-15 00:00:00.000")
-                }
-            );
+            //modelBuilder.Entity<Product>().HasData(
+            //    new Product
+            //    {
+            //        ProductID = 1,
+            //        ProductCode = "DRAFT10",
+            //        Name = "Draft Manager 1.0",
+            //        YearlyPrice = 4.99M,
+            //        ReleaseDate = DateTime.Parse("2017-02-01")
+            //    },
+            //    new Product
+            //    {
+            //        ProductID = 2,
+            //        ProductCode = "DRAFT20",
+            //        Name = "Draft Manager 2.0",
+            //        YearlyPrice = 5.99M,
+            //        ReleaseDate = DateTime.Parse("2019-07-15 00:00:00.000")
+            //    },
+            //    new Product
+            //    {
+            //        ProductID = 3,
+            //        ProductCode = "LEAG10",
+            //        Name = "League Scheduler 1.0",
+            //        YearlyPrice = 4.99M,
+            //        ReleaseDate = DateTime.Parse("2016-05-01 00:00:00.000")
+            //    },
+            //    new Product
+            //    {
+            //        ProductID = 4,
+            //        ProductCode = "LEAGD10",
+            //        Name = "League Scheduler Deluxe 1.0",
+            //        YearlyPrice = 7.99M,
+            //        ReleaseDate = DateTime.Parse("2016-08-01 00:00:00.000")
+            //    },
+            //    new Product
+            //    {
+            //        ProductID = 5,
+            //        ProductCode = "TEAM10",
+            //        Name = "Team Manager 1.0",
+            //        YearlyPrice = 4.99M,
+            //        ReleaseDate = DateTime.Parse("2017-05-01 00:00:00.000")
+            //    },
+            //    new Product
+            //    {
+            //        ProductID = 6,
+            //        ProductCode = "TRNY10",
+            //        Name = "Tournament Master 1.0",
+            //        YearlyPrice = 4.99M,
+            //        ReleaseDate = DateTime.Parse("2015-12-01 00:00:00.000")
+            //    },
+            //    new Product
+            //    {
+            //        ProductID = 7,
+            //        ProductCode = "TRNY20",
+            //        Name = "Tournament Master 2.0",
+            //        YearlyPrice = 5.99M,
+            //        ReleaseDate = DateTime.Parse("2018-02-15 00:00:00.000")
+            //    }
+            //);
 
-            modelBuilder.Entity<Technician>().HasData(
-                new Technician
-                {
-                    TechnicianID = 11,
-                    Name = "Alison Diaz",
-                    Email = "alison@sportsprosoftware.com",
-                    Phone = "800-555-0443"
-                },
-                new Technician
-                {
-                    TechnicianID = 12,
-                    Name = "Jason Lee",
-                    Email = "jason@sportsprosoftware.com",
-                    Phone = "800-555-0444"
-                },
-                new Technician
-                {
-                    TechnicianID = 13,
-                    Name = "Andrew Wilson",
-                    Email = "awilson@sportsprosoftware.com",
-                    Phone = "800-555-0449"
-                },
-                new Technician
-                {
-                    TechnicianID = 14,
-                    Name = "Gunter Wendt",
-                    Email = "gunter@sportsprosoftware.com",
-                    Phone = "800-555-0400"
-                },
-                new Technician
-                {
-                    TechnicianID = 15,
-                    Name = "Gina Fiori",
-                    Email = "gfiori@sportsprosoftware.com",
-                    Phone = "800-555-0459"
-                }
-            );
+            //modelBuilder.Entity<Technician>().HasData(
+            //    new Technician
+            //    {
+            //        TechnicianID = 11,
+            //        Name = "Alison Diaz",
+            //        Email = "alison@sportsprosoftware.com",
+            //        Phone = "800-555-0443"
+            //    },
+            //    new Technician
+            //    {
+            //        TechnicianID = 12,
+            //        Name = "Jason Lee",
+            //        Email = "jason@sportsprosoftware.com",
+            //        Phone = "800-555-0444"
+            //    },
+            //    new Technician
+            //    {
+            //        TechnicianID = 13,
+            //        Name = "Andrew Wilson",
+            //        Email = "awilson@sportsprosoftware.com",
+            //        Phone = "800-555-0449"
+            //    },
+            //    new Technician
+            //    {
+            //        TechnicianID = 14,
+            //        Name = "Gunter Wendt",
+            //        Email = "gunter@sportsprosoftware.com",
+            //        Phone = "800-555-0400"
+            //    },
+            //    new Technician
+            //    {
+            //        TechnicianID = 15,
+            //        Name = "Gina Fiori",
+            //        Email = "gfiori@sportsprosoftware.com",
+            //        Phone = "800-555-0459"
+            //    }
+            //);
 
             modelBuilder.Entity<Country>().HasData(
                 new Country { CountryID = "AU", Name = "Australia" },
