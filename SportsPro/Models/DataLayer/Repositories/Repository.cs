38619011562b1dedc0e6b3c64922a1b245e817d0
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-22 18:19:25 -0600

    Repository updated

--- SportsPro/Models/DataLayer/Repositories/Repository.cs
+++ SportsPro/Models/DataLayer/Repositories/Repository.cs
@@ -1,25 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsPro.Models
 {
-    public interface IRepository<T> where T: class
+    public class Repository<T> : IRepository<T> where T: class
     {
-        IEnumerable<T> List(QueryOptions<T> options);
+        protected SportsProContext context { get; set; }
+        private DbSet<T> dbset { get; set; }
+        public Repository(SportsProContext ctx)
+        {
+            context = ctx;
+            dbset = context.Set<T>();
+        }
 
-        int Count { get; }
+        // get number of retrieved entities - use private backing field bc when 
+        // filtering, count might be less than dbset.Count()
+        private int? count;
+        public int Count => count ?? dbset.Count();
 
-        // overloaded Get() method
-        T Get(QueryOptions<T> options);
-        T Get(int id);
-        T Get(string id);
+        // retrieve a list of entities
+        public virtual IEnumerable<T> List(QueryOptions<T> options)
+        {
+            IQueryable<T> query = BuildQuery(options);
+            return query.ToList();
+        }
 
-        void Insert(T entity);
-        void Update(T entity);
-        void Delete(T entity);
+        // retrieve a single entity (3 overloads)
+        public virtual T Get(int id) => dbset.Find(id);
+        public virtual T Get(string id) => dbset.Find(id);
+        public virtual T Get(QueryOptions<T> options)
+        {
+            IQueryable<T> query = BuildQuery(options);
+            return query.FirstOrDefault();
+        }
 
-        void Save();
+        // insert, update, delete, save
+        public virtual void Insert(T entity) => dbset.Add(entity);
+        public virtual void Update(T entity) => dbset.Update(entity);
+        public virtual void Delete(T entity) => dbset.Remove(entity);
+        public virtual void Save() => context.SaveChanges();
+
+        // private helper method to build query expression
+        private IQueryable<T> BuildQuery(QueryOptions<T> options)
+        {
+            IQueryable<T> query = dbset;
+            foreach (string include in options.GetIncludes())
+            {
+                query = query.Include(include);
+            }
+            if (options.HasWhere)
+            {
+                foreach (var clause in options.WhereClauses)
+                {
+                    query = query.Where(clause);
+                }
+                count = query.Count(); // get filtered count
+            }
+            if (options.HasOrderBy)
+            {
+                if (options.OrderByDirection == "asc")
+                    query = query.OrderBy(options.OrderBy);
+                else
+                    query = query.OrderByDescending(options.OrderBy);
+            }
+            if (options.HasPaging)
+                query = query.PageBy(options.PageNumber, options.PageSize);
+
+            return query;
+        }
     }
 }
