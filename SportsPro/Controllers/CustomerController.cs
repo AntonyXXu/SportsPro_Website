3f1f10215983747091a51c676f63acae502aaf3f
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-22 21:57:45 -0600

    Resolved db links, using repo

--- SportsPro/Controllers/CustomerController.cs
+++ SportsPro/Controllers/CustomerController.cs
@@ -10,39 +10,38 @@ namespace SportsPro.Controllers
     public class CustomerController : Controller
     {
         private SportsProContext context { get; set; }
-        private List<Country> countries { get; set; }
-        private Repository<Country> countriesRep { get; set; }
+        private List<Country> oldcountries { get; set; }
+        private Repository<Country> countries { get; set; }
         private Repository<Customer> customers { get; set; }
         public CustomerController(SportsProContext ctx)
         {
             context = ctx;
-            countries = context.Countries.ToList();
-            countriesRep = new Repository<Country>(ctx);
+            oldcountries = context.Countries.ToList();
+            countries = new Repository<Country>(ctx);
             customers = new Repository<Customer>(ctx);
         }
 
         [Route("customers")]
         public IActionResult List()
         {
-            List<Customer> oldcustomers = context.Customers.ToList();
-
+            //List<Customer> oldcustomers = context.Customers.ToList();
             IEnumerable<Customer> cust = customers.List(new QueryOptions<Customer>());
-
             return View(cust);
         }
 
         [HttpGet]
         public IActionResult Add()
         {
-            ViewBag.Country = countries;
-            return View(new Customer());
+            ViewBag.Country = countries.List(new QueryOptions<Country>());
+            Customer cust = new Customer();
+            return View(cust);
         }
 
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            ViewBag.Country = countries;
-            Customer cust = context.Customers.Find(id);
+            ViewBag.Country = countries.List(new QueryOptions<Country>());
+            Customer cust = customers.Get(id);
             return View(cust);
         }
 
@@ -51,8 +50,8 @@ namespace SportsPro.Controllers
         {
             try
             {
-                context.Customers.Add(cust);
-                context.SaveChanges();
+                customers.Insert(cust);
+                customers.Save();
                 return RedirectToAction("List", "Customer");
             }
             catch
@@ -66,13 +65,15 @@ namespace SportsPro.Controllers
         {
             try
             {
-                context.Customers.Update(cust);
-                context.SaveChanges();
+                customers.Insert(cust);
+                customers.Save();
+                //context.Customers.Update(cust);
+                //context.SaveChanges();
                 return RedirectToAction("List", "Customer");
             }
             catch
             {
-                return View();
+                return View(cust);
             }
         }
 
@@ -81,9 +82,12 @@ namespace SportsPro.Controllers
         {
             try
             {
-                Customer cust = context.Customers.Find(id);
-                context.Customers.Remove(cust);
-                context.SaveChanges();
+                Customer cust = customers.Get(id);
+                customers.Delete(cust);
+                customers.Save();
+                //Customer cust = context.Customers.Find(id);
+                //context.Customers.Remove(cust);
+                //context.SaveChanges();
                 return RedirectToAction("List", "Customer");
             }
             catch
