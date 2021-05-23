25b3ab2444439f3cede0d79b959b7dcd6e8bb5af
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-22 18:12:53 -0600

    Update QueryExtensions.cs

--- SportsPro/Models/DataLayer/QueryExtensions.cs
+++ SportsPro/Models/DataLayer/QueryExtensions.cs
@@ -1,55 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
+﻿using System.Linq;
 
-namespace SportsPro.Models.DataLayer
+namespace Bookstore.Models
 {
-    public class QueryOptions<T>
+    public static class QueryExtensions
     {
-        // public properties for sorting, filtering, and paging
-        public Expression<Func<T, Object>> OrderBy { get; set; }
-        public string OrderByDirection { get; set; } = "asc";  // default
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-
-        // filter by more than one where clause. User can set value for Where property 
-        // repeatedly, or can instantiate a new WhereClauses property, whose type
-        // is a class that inherits a list of where expressions (see below)
-        public WhereClauses<T> WhereClauses { get; set; }
-        public Expression<Func<T, bool>> Where
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> items,
+            int pagenumber, int pagesize)
         {
-            set
-            {
-                if (WhereClauses == null)
-                {
-                    WhereClauses = new WhereClauses<T>();
-                }
-                WhereClauses.Add(value);
-            }
+            return items
+                .Skip((pagenumber - 1) * pagesize)
+                .Take(pagesize);
         }
-
-        // private backing field for property and method that work with Include strings
-        private string[] includes;
-
-        // public write-only property for Include strings – converts comma-separated string to array 
-        // and stores in private backing field
-        public string Includes
-        {
-            set => includes = value.Replace(" ", "").Split(',');
-        }
-
-        // public get method for Include strings - returns private backing field or empty string array 
-        // if private backing field is null
-        public string[] GetIncludes() => includes ?? new string[0];
-
-        // read-only properties 
-        public bool HasWhere => WhereClauses != null;
-        public bool HasOrderBy => OrderBy != null;
-        public bool HasPaging => PageNumber > 0 && PageSize > 0;
     }
-
-    // basically an alias for a list of where expressions - to make code clearer
-    public class WhereClauses<T> : List<Expression<Func<T, bool>>> { }
-
 }
-
